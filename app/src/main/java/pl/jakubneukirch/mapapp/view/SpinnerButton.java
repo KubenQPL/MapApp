b37371ce51d014237bad81f3949f4ab422f0a63b
JakubNeukirch <jakub.neukirch@gmail.com>
2018-06-12 22:15:00 +0200

    Add foreground support below api 23

--- app/src/main/java/pl/jakubneukirch/mapapp/view/SpinnerButton.java
+++ app/src/main/java/pl/jakubneukirch/mapapp/view/SpinnerButton.java
@@ -3,8 +3,11 @@ package pl.jakubneukirch.mapapp.view;
 import android.app.Service;
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatSpinner;
 import android.util.AttributeSet;
@@ -17,13 +20,16 @@ import pl.jakubneukirch.mapapp.R;
 
 public class SpinnerButton extends AppCompatSpinner {
 
-    private static final int SPINNER_STYLE = R.style.AppTheme_Spinner;
     private static final int[] STATE_EXPANDED = {R.attr.state_expanded};
 
     private boolean isExpanded = false;
     public String[] items = null;
     Point displaySize = new Point();
+
     private OnItemSelectedListener onItemSelectedListener;
+
+    private int foregroundResId = -1;
+    private Drawable foregroundDrawable = null;
 
     public SpinnerButton(Context context) {
         super(context);
@@ -37,26 +43,63 @@ public class SpinnerButton extends AppCompatSpinner {
 
     public SpinnerButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        if (Build.VERSION.SDK_INT < 23) {
+            foregroundResId = getForegroundResIdAttribute(attrs, 0);
+        }
         setup();
     }
 
     public SpinnerButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        if (Build.VERSION.SDK_INT < 23) {
+            foregroundResId = getForegroundResIdAttribute(attrs, defStyleAttr);
+        }
         setup();
     }
 
     public SpinnerButton(Context context, AttributeSet attrs, int defStyleAttr, int mode) {
         super(context, attrs, defStyleAttr, mode);
+        if (Build.VERSION.SDK_INT < 23) {
+            foregroundResId = getForegroundResIdAttribute(attrs, defStyleAttr);
+        }
         setup();
     }
 
     public SpinnerButton(Context context, AttributeSet attrs, int defStyleAttr, int mode, Resources.Theme popupTheme) {
         super(context, attrs, defStyleAttr, mode, popupTheme);
+        if (Build.VERSION.SDK_INT < 23) {
+            foregroundResId = getForegroundResIdAttribute(attrs, defStyleAttr);
+        }
         setup();
     }
 
     private void setup() {
+        if(Build.VERSION.SDK_INT >= 23){
+            setPopupBackgroundDrawable(null);
+        } else {
+            loadForegroundDrawable();
+        }
+    }
 
+    private void loadForegroundDrawable() {
+        if (foregroundResId != -1) {
+            foregroundDrawable = getResources().getDrawable(foregroundResId);
+        }
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        foregroundDrawable.setState(getDrawableState());
+        invalidate();
+    }
+
+    private int getForegroundResIdAttribute(AttributeSet attrs, int defStyle) {
+        final int[] foregroundAttr = {android.R.attr.foreground};
+        TypedArray array = getContext().obtainStyledAttributes(attrs, foregroundAttr, defStyle, 0);
+        int foregroundRes = array.getResourceId(0, -1);
+        array.recycle();
+        return foregroundRes;
     }
 
     public void setItems(String[] items) {
@@ -70,6 +113,9 @@ public class SpinnerButton extends AppCompatSpinner {
             getSelectedView().setVisibility(View.GONE);
         }
         super.onDraw(canvas);
+        if (Build.VERSION.SDK_INT < 23 && foregroundDrawable != null) {
+            foregroundDrawable.draw(canvas);
+        }
     }
 
     @Override
@@ -118,6 +164,7 @@ public class SpinnerButton extends AppCompatSpinner {
         final Display display = ((WindowManager) getContext().getSystemService(Service.WINDOW_SERVICE)).getDefaultDisplay();
         display.getSize(displaySize);
         setDropDownWidth(displaySize.x);
+        foregroundDrawable.setBounds(0,0,getWidth(), getHeight());
     }
 
     public void setExpanded(boolean expanded) {
